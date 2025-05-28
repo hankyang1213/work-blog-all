36eb2b527e34f5ba7e9824312bd5f91b99483d5b
hankyang1213 <47803174+hankyang1213@users.noreply.github.com>
2025-05-29 00:44:38 +0800

    homework
    party2

---
+++ all.js
@@ -0,0 +1,19 @@
+/*var cot = 0;
+    function right(){
+        if(cot <= 3){
+           $('.one').eq(cot).animate({'margin-left':'-999px'},999);
+            cot ++;
+        }
+    }
+    function pre(){
+        if(cot > -1){
+            $('.one').eq(cot).animate({'margin-left':'0px'},999);
+            cot--;
+        }
+    }*/
+
+        var a=8;
+        function right(){
+            $('.serveplan .title').animate({'font-size':'50px'})                
+        }
+        
